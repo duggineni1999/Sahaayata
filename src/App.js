851b3345e0534d00c3e28021b78c0a3df199bb38
Valmiki Saidushyanth <150247676+SaidushyanthValmiki@users.noreply.github.com>
2024-05-22 09:22:10 +0530

    Updated Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./Footer/Footer";
+
+const HomePage = () => <div className="p-5">Home Page</div>;
+const AboutPage = () => <div className="p-5">About Page</div>;
+const ImmediatePage = () => <div className="p-5">Immediate Page</div>;
+const WorkshopsPage = () => <div className="p-5">Workshops Page</div>;
+const MediaPage = () => <div className="p-5">Media Page</div>;
+const GalleryPage = () => <div className="p-5">Gallery Page</div>;
+const DonatePage = () => <div className="p-5">Donate Page</div>;
+const ContactPage = () => <div className="p-5">Contact Page</div>;
+const TermsUse = () => <div className="p-5">Terms Use</div>;
+const Privacy = () => <div className="p-5">Contact Page</div>;
+const Cookie = () => <div className="p-5">Contact Page</div>;
+const Refund = () => <div className="p-5">Contact Page</div>;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/immediate" element={<ImmediatePage />} />
+          <Route path="/workshops" element={<WorkshopsPage />} />
+          <Route path="/media" element={<MediaPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/donate" element={<DonatePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/terms" element={<TermsUse />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/cookie" element={<Cookie />} />
+          <Route path="/refund" element={<Refund />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
